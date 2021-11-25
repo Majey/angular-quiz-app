df53e48800881cec39c7a7d46ea1bc191d168e0e
Majey <kevinerick78@yahoo.com>
2021-11-25 21:05:23 +0300

    injecting question.json and configuring the questions, options/choices and previous/next button logic

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from '../service/questions.service';
 
 @Component({
   selector: 'app-question',
@@ -8,11 +9,34 @@ import { Component, OnInit } from '@angular/core';
 export class QuestionComponent implements OnInit {
 
   public name:string = "";
+  public questionList: any = [];
+  public currentQuestion: number = 0;
+  public points: number = 0;
+  counter: number = 60;
 
-  constructor() { }
+  constructor(private questionService: QuestionsService) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
+    this.getAllQuestions()
+  }
+
+  getAllQuestions(){
+    this.questionService.getQuestionsJson()
+    .subscribe(res => {
+      this.questionList = res.questions;
+    })
+  }
+
+  nextQuestion(){
+    if (this.currentQuestion<8){
+      this.currentQuestion ++
+    }
+  }
+  previousQuestion(){
+    if (this.currentQuestion>0){
+      this.currentQuestion --
+    }
   }
 
 }
